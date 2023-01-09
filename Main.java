42e4553466d5ef01061e445582c95ee4bbf45398
nicolly ketlyn <nicollyketlyn97@gmail.com>
2023-01-09 09:58:18 -0300

    atualizaçao

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloWorldSwing");
+        final JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
         ControleRemoto controleRemoto = new ControleRemoto();
 
         controleRemoto.ligar();
@@ -7,5 +14,6 @@ public class Main {
         controleRemoto.play();
         controleRemoto.abrirMenu();
         controleRemoto.fecharMenu();
+
     }
 }

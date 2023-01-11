ac7698225b2730cd25da0974cfeede5f89d6a001
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-01-11 15:25:47 -0300

    atualização

--- Main.java
+++ Main.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ControleRemot controleRemoto = new ControleRemot();
+        ControleRemoto controleRemoto = new ControleRemoto();
         Scanner ler = new Scanner(System.in);
         int sim = 1;
         int nao = 2;
-        String resposta;
+        int resposta;
 
         System.out.println("Você gostaria de ligar a TV?\n1-Sim\n2-Não");
-        resposta = ler.nextLine();
-        int numero = Integer.parseInt(resposta);
-        if(numero == sim){
+        resposta = ler.nextInt();
+        if(resposta == sim){
             System.out.println("TV Ligada!");
             controleRemoto.abrirMenu();
-        } else if(numero == nao){
+        } else if(resposta == nao){
             System.out.println("TV desligada!");
+        }
+        System.out.println("Voltar ao menu?\n1-Sim\n2-Não");
+        resposta = ler.nextInt();
+        if(resposta == sim){
+            controleRemoto.abrirMenu();
+        } else{
+            controleRemoto.fecharMenu();
         }
     }
 }

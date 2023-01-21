7f14201c9d924f83e5cce0b0fbdca68feea67d61
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-01-21 11:44:40 -0300

    Atualização 1.6

--- Treino/Interface.java
+++ Treino/Interface.java
@@ -1,22 +1,31 @@
 package Treino;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
 
 public class Interface extends JFrame{
-    private JButton ligarTv, desligarTv;
-    private JButton mudarCanal, ligarMudo, desligarMudo, aumentarVolume, baixarVolume, play, pause, fecharMenu;
+    private JButton ligarTv, desligarTv,
+            mudarCanal, ligarMudo, desligarMudo,
+            aumentarVolume, baixarVolume, play,
+            pause, fecharMenu;
 
     public Interface(){
+
         // Cria os botões iniciais e adiciona os eventos de clique
-        Border thickBorder = new LineBorder(Color.WHITE, 12);
-        ligarTv = new JButton();
-        ligarTv.setBackground(new Color(255,99,71));
-        ligarTv.setIcon(new ImageIcon("Treino/botao-ligar-desligar.png"));
+        ImageIcon ligar = new ImageIcon("Treino/Imagens/botao-ligar-desligar.png");
+        ligar.setImage(ligar.getImage().getScaledInstance(50, 50, 100));
+        ligarTv = new JButton(ligar);
+        ligarTv.setBackground(Color.red);
         ligarTv.addActionListener(new OnButtonListener());
-        desligarTv = new JButton("Desligar TV");
+        ImageIcon desligar = new ImageIcon("Treino/Imagens/desligar.png");
+        desligar.setImage(desligar.getImage().getScaledInstance(50, 50, 100));
+        desligarTv = new JButton(desligar);
+        desligarTv.setBackground(Color.WHITE);
         desligarTv.addActionListener(new OffButtonListener());
 
         // Adiciona os botões iniciais à interface
@@ -33,18 +42,37 @@ public class Interface extends JFrame{
     }
     private class OnButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+
             // Remove os botões iniciais
             remove(ligarTv);
             remove(desligarTv);
+
             //Cria os novos botões
-            mudarCanal = new JButton("Mudar de canal");
-            ligarMudo = new JButton("Ligar Mudo");
-            desligarMudo = new JButton("Desligar mudo");
-            aumentarVolume = new JButton("Aumentar volume");
-            baixarVolume = new JButton("Baixar volume");
-            play = new JButton("Play");
-            pause = new JButton("Pause");
-            fecharMenu = new JButton("Fechar Menu");
+            ImageIcon mudar = new ImageIcon("Treino/Imagens/botao-pass-track.png");
+            mudar.setImage(mudar.getImage().getScaledInstance(50, 50, 100));
+            mudarCanal = new JButton(mudar);
+            ImageIcon liMudo = new ImageIcon("Treino/Imagens/mudo.png");
+            liMudo.setImage(liMudo.getImage().getScaledInstance(50, 50, 100));
+            ligarMudo = new JButton(liMudo);
+            ImageIcon desMudo = new ImageIcon("Treino/Imagens/botao-de-volume.png");
+            desMudo.setImage(desMudo.getImage().getScaledInstance(50, 50, 100));
+            desligarMudo = new JButton(desMudo);
+            ImageIcon aumeVolu = new ImageIcon("Treino/Imagens/aumentar-o-volume.png");
+            aumeVolu.setImage(aumeVolu.getImage().getScaledInstance(50, 50, 100));
+            aumentarVolume = new JButton(aumeVolu);
+            ImageIcon baiVolu = new ImageIcon("Treino/Imagens/volume-baixo.png");
+            baiVolu.setImage(baiVolu.getImage().getScaledInstance(50, 50, 100));
+            baixarVolume = new JButton(baiVolu);
+            ImageIcon playIma = new ImageIcon("Treino/Imagens/botao-play.png");
+            playIma.setImage(playIma.getImage().getScaledInstance(50, 50, 100));
+            play = new JButton(playIma);
+            ImageIcon pauseIma = new ImageIcon("Treino/Imagens/botao-de-pausa-de-video.png");
+            pauseIma.setImage(pauseIma.getImage().getScaledInstance(50, 50, 100));
+            pause = new JButton(pauseIma);
+            ImageIcon fechaMenu = new ImageIcon("Treino/Imagens/botao-excluir.png");
+            fechaMenu.setImage(fechaMenu.getImage().getScaledInstance(50, 50, 100));
+            fecharMenu = new JButton(fechaMenu);
+            setBackground(Color.WHITE);
 
         // Adiciona os novos botões à interface
             add(mudarCanal);
@@ -63,12 +91,14 @@ public class Interface extends JFrame{
             pause.addActionListener(new pause());
             add(fecharMenu);
             fecharMenu.addActionListener(new fechar());
+
             //Atualiza a tela
             revalidate();
             repaint();
         }
     }
 
+    //Dá as ações aos botões
     private class mudarCanal implements ActionListener {
         public void actionPerformed(ActionEvent e){
             ControleRemoto cr = new ControleRemoto();
@@ -119,7 +149,6 @@ public class Interface extends JFrame{
     }
     private class OffButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            //Adicionar ação do botão Desligar aqui
             ControleRemoto cr = new ControleRemoto();
             desligarTv.addActionListener(cr.desligar());
         }

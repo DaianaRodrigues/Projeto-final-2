550b9f241e7dd6fa4fd63a49885c482127332de3
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-01-22 14:53:25 -0300

    Atualização 1.7

--- Treino/Interface.java
+++ Treino/Interface.java
@@ -15,17 +15,19 @@ public class Interface extends JFrame{
             pause, fecharMenu;
 
     public Interface(){
-
         // Cria os botões iniciais e adiciona os eventos de clique
         ImageIcon ligar = new ImageIcon("Treino/Imagens/botao-ligar-desligar.png");
         ligar.setImage(ligar.getImage().getScaledInstance(50, 50, 100));
         ligarTv = new JButton(ligar);
         ligarTv.setBackground(Color.red);
+        ligarTv.setBorder(null);
+        ligarTv.setBounds(0,0,50,50);
         ligarTv.addActionListener(new OnButtonListener());
         ImageIcon desligar = new ImageIcon("Treino/Imagens/desligar.png");
         desligar.setImage(desligar.getImage().getScaledInstance(50, 50, 100));
         desligarTv = new JButton(desligar);
         desligarTv.setBackground(Color.WHITE);
+        desligarTv.setBorder(null);
         desligarTv.addActionListener(new OffButtonListener());
 
         // Adiciona os botões iniciais à interface
@@ -37,7 +39,8 @@ public class Interface extends JFrame{
         setSize(500, 300);
         setTitle("Controle Remoto");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(layout);
+        setLocation(400,200);
+        setLayout(new GridLayout(2,2));
         setVisible(true);
     }
     private class OnButtonListener implements ActionListener {
@@ -51,28 +54,43 @@ public class Interface extends JFrame{
             ImageIcon mudar = new ImageIcon("Treino/Imagens/botao-pass-track.png");
             mudar.setImage(mudar.getImage().getScaledInstance(50, 50, 100));
             mudarCanal = new JButton(mudar);
+            mudarCanal.setBorder(null);
+            mudarCanal.setBackground(Color.WHITE);
             ImageIcon liMudo = new ImageIcon("Treino/Imagens/mudo.png");
             liMudo.setImage(liMudo.getImage().getScaledInstance(50, 50, 100));
             ligarMudo = new JButton(liMudo);
+            ligarMudo.setBorder(null);
+            ligarMudo.setBackground(Color.WHITE);
             ImageIcon desMudo = new ImageIcon("Treino/Imagens/botao-de-volume.png");
             desMudo.setImage(desMudo.getImage().getScaledInstance(50, 50, 100));
             desligarMudo = new JButton(desMudo);
+            desligarMudo.setBorder(null);
+            desligarMudo.setBackground(Color.WHITE);
             ImageIcon aumeVolu = new ImageIcon("Treino/Imagens/aumentar-o-volume.png");
             aumeVolu.setImage(aumeVolu.getImage().getScaledInstance(50, 50, 100));
             aumentarVolume = new JButton(aumeVolu);
+            aumentarVolume.setBorder(null);
+            aumentarVolume.setBackground(Color.WHITE);
             ImageIcon baiVolu = new ImageIcon("Treino/Imagens/volume-baixo.png");
             baiVolu.setImage(baiVolu.getImage().getScaledInstance(50, 50, 100));
             baixarVolume = new JButton(baiVolu);
+            baixarVolume.setBorder(null);
+            baixarVolume.setBackground(Color.WHITE);
             ImageIcon playIma = new ImageIcon("Treino/Imagens/botao-play.png");
             playIma.setImage(playIma.getImage().getScaledInstance(50, 50, 100));
             play = new JButton(playIma);
+            play.setBorder(null);
+            play.setBackground(Color.WHITE);
             ImageIcon pauseIma = new ImageIcon("Treino/Imagens/botao-de-pausa-de-video.png");
             pauseIma.setImage(pauseIma.getImage().getScaledInstance(50, 50, 100));
             pause = new JButton(pauseIma);
+            pause.setBorder(null);
+            pause.setBackground(Color.WHITE);
             ImageIcon fechaMenu = new ImageIcon("Treino/Imagens/botao-excluir.png");
             fechaMenu.setImage(fechaMenu.getImage().getScaledInstance(50, 50, 100));
             fecharMenu = new JButton(fechaMenu);
-            setBackground(Color.WHITE);
+            fecharMenu.setBorder(null);
+            fecharMenu.setBackground(Color.WHITE);
 
         // Adiciona os novos botões à interface
             add(mudarCanal);
